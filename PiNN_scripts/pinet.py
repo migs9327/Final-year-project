882e31b0ac70e6099a19c06411dcd6506cc0e201
migs9327 <50503456+migs9327@users.noreply.github.com>
2023-05-03 16:24:06 +0100

    Supporting Info

---
+++ PiNN_scripts/pinet.py
@@ -0,0 +1,361 @@
+# -*- coding: utf-8 -*-
+import sys
+import tensorflow as tf
+import numpy as np
+from pinn.utils import pi_named, connect_dist_grad
+from pinn.layers import CellListNL, CutoffFunc, \
+    PolynomialBasis, GaussianBasis, AtomicOnehot, ANNOutput
+
+class FFLayer(tf.keras.layers.Layer):
+    """Feed-forward layer, a shortcut for constructing multiple layers
+
+    Args:
+        n_node (list): dimension of the layers
+        act: activation function of the layers
+        name: name of the layer
+
+    Returns:
+        Nodes after the fc layers
+    """
+    def __init__(self, n_nodes=[64, 64], **kwargs):
+        super(FFLayer, self).__init__()
+        self.dense_layers = [tf.keras.layers.Dense(
+            n_node, **kwargs) for n_node in n_nodes]
+
+    def call(self, tensor):
+        for layer in self.dense_layers:
+            tensor = layer(tensor)
+        return tensor
+
+class PILayer(tf.keras.layers.Layer):
+    """PiNN style interaction layer
+
+    Args:
+        n_nodes: number of nodes to use
+            Note that the last element of n_nodes specifies the dimention of
+            the fully connected network before applying the basis function.
+            Dimension of the last node is [pairs*n_nodes[-1]*n_basis], the
+            output is then summed with the basis to form the interaction nodes
+        **kwargs: keyword arguments will be parsed to the feed forward layers
+    """
+    def __init__(self, n_nodes=[64], **kwargs):
+        super(PILayer, self).__init__()
+        self.n_nodes = n_nodes
+        self.kwargs = kwargs
+
+    def build(self, shapes):
+        self.n_basis = shapes[2][-1]
+        n_nodes_iter = self.n_nodes.copy()
+        n_nodes_iter[-1] *= self.n_basis
+        self.ff_layer = FFLayer(n_nodes_iter, **self.kwargs)
+
+    def call(self, tensors):
+        ind_2, prop, basis = tensors
+        ind_i = ind_2[:, 0]
+        ind_j = ind_2[:, 1]
+        # print(prop)
+        prop_i = tf.gather(prop, ind_i)
+        prop_j = tf.gather(prop, ind_j)
+
+        inter = tf.concat([prop_i, prop_j], axis=-1)
+        inter = self.ff_layer(inter)
+        inter = tf.reshape(inter, tf.concat(
+            [tf.shape(inter)[:-1], [self.n_nodes[-1], self.n_basis]], 0))
+        inter = tf.reduce_sum(inter*basis, axis=-1)
+        return inter
+
+
+class IPLayer(tf.keras.layers.Layer):
+    """PiNet style IP layer
+
+    transforms pairwise interactions to atomic properties"""
+    def __init__(self):
+        super(IPLayer, self).__init__()
+
+    def call(self, tensors):
+        ind_2, inter = tensors
+        n_atoms = tf.reduce_max(ind_2) + 1
+
+        # print("ind_2 shape:", ind_2.shape)
+        # print("inter shape:", inter.shape)
+
+        # print("n_atoms (IPLayer):", n_atoms)
+        # print("ind_2 values (IPLayer):", ind_2.numpy())
+        # unique_indices = np.unique(ind_2)
+        # print("Unique indices:", unique_indices)
+        # print("Number of unique indices:", len(unique_indices))
+        # missing_index = set(range(300)) - set(unique_indices)
+        # print("Missing index:", missing_index)
+
+        prop = tf.math.unsorted_segment_sum(inter, ind_2[:, 0], n_atoms)
+
+        # print("prop shape after unsorted_segment_sum:", prop.shape)
+
+        return prop
+
+
+class OutLayer(tf.keras.layers.Layer):
+    """PiNet style output layer
+
+    generates outputs from atomic properties after each GC block"""
+
+    def __init__(self, n_nodes, out_units, **kwargs):
+        super(OutLayer, self).__init__()
+        self.out_units = out_units
+        self.ff_layer = FFLayer(n_nodes, **kwargs)
+        self.out_units = tf.keras.layers.Dense(
+            out_units, activation=None, use_bias=False, name='noact')
+
+    def call(self, tensors):
+        ind_1, prop, prev_output = tensors
+        prop = self.ff_layer(prop)
+        output = self.out_units(prop) + prev_output
+        return output
+
+class GCBlock(tf.keras.layers.Layer):
+    def __init__(self, pp_nodes, pi_nodes, ii_nodes, **kwargs):
+        super(GCBlock, self).__init__()
+        iiargs = kwargs.copy()
+        iiargs.update(use_bias=False)
+        self.pp_layer = FFLayer(pp_nodes, **kwargs)
+        self.pi_layer = PILayer(pi_nodes, **kwargs)
+        self.ii_layer = FFLayer(ii_nodes, **iiargs)
+        self.ip_layer = IPLayer()
+
+    def call(self, tensors):
+        ind_2, prop, basis = tensors
+        n_atoms = tf.shape(prop)[0]
+        # print("n_atoms (GCBlock):", n_atoms)
+
+        # print(f"prop shape before pp_layer: {prop.shape}")
+        prop = self.pp_layer(prop)
+        # print(f"prop shape after pp_layer: {prop.shape}")
+        
+        # print(f"inter shape before pi_layer: {prop.shape}")
+        inter = self.pi_layer([ind_2, prop, basis])
+        # print(f"inter shape after pi_layer: {inter.shape}")
+        
+        # print(f"inter shape before ii_layer: {inter.shape}")
+        inter = self.ii_layer(inter)
+        # print(f"inter shape after ii_layer: {inter.shape}")
+        
+        # print(f"prop shape before ip_layer: {prop.shape}")
+        prop = self.ip_layer([ind_2, inter])
+        # print(f"prop shape after ip_layer: {prop.shape}")
+
+        return prop
+
+class ResUpdate(tf.keras.layers.Layer):
+    def __init__(self):
+       super(ResUpdate, self).__init__()
+
+    def build(self, shapes):
+        assert isinstance(shapes, list) and len(shapes)==2
+        if shapes[0][-1] == shapes[1][-1]:
+            self.transform = lambda x:x
+        else:
+            self.transform = tf.keras.layers.Dense(
+                shapes[1][-1], use_bias=False, activation=None)
+
+    def call(self, tensors):
+        old, new = tensors
+        # print("Shape of 'old' tensor:", old.shape)
+        # print("Shape of 'new' tensor:", new.shape)
+        return self.transform(old) + new
+
+
+class PreprocessLayer(tf.keras.layers.Layer):
+    def __init__(self, atom_types, rc):
+        super(PreprocessLayer, self).__init__()
+        self.embed = AtomicOnehot(atom_types)
+        self.nl_layer = CellListNL(rc)
+
+    def call(self, tensors):
+        tensors = tensors.copy()
+        for k in ['elems', 'dist']:
+            if k in tensors.keys():
+                if tf.shape(tensors[k])[0] != tf.shape(tensors[k])[-1]:
+                    print(f"Shape mismatch in {k}: {tf.shape(tensors[k])}")
+                tensors[k] = tf.reshape(tensors[k], tf.shape(tensors[k])[:1])
+
+        # Check input tensors before processing
+        # print("=== Input tensors ===")
+        # for key, value in tensors.items():
+        #     print(f"{key}: {value.shape}")
+
+        if 'ind_2' not in tensors:
+            tensors.update(self.nl_layer(tensors))
+            tensors['prop'] = tf.cast(
+                self.embed(tensors['elems']), tensors['coord'].dtype)
+
+        # Assert and print output tensors after processing
+        # print("=== Output tensors ===")
+        # for key in ['ind_1', 'ind_2', 'prop', 'dist']:
+        #     assert key in tensors, f"{key} not found in tensors"
+        #     print(f"{key} shape: {tensors[key].shape}")
+
+        return tensors
+
+# class PreprocessLayer(tf.keras.layers.Layer):
+#     def __init__(self, atom_types, rc):
+#         super(PreprocessLayer, self).__init__()
+#         self.embed = AtomicOnehot(atom_types)
+#         self.nl_layer = CellListNL(rc)
+
+#     def call(self, tensors):
+#         tensors = tensors.copy()
+#         for k in ['elems', 'dist']:
+#             if k in tensors.keys():
+#                 if tf.shape(tensors[k])[0] != tf.shape(tensors[k])[-1]:
+#                     print(f"Shape mismatch in {k}: {tf.shape(tensors[k])}")
+#                 tensors[k] = tf.reshape(tensors[k], tf.shape(tensors[k])[:1])
+#         if 'ind_2' not in tensors:
+#             tensors.update(self.nl_layer(tensors))
+#             tensors['prop'] = tf.cast(
+#                 self.embed(tensors['elems']), tensors['coord'].dtype)
+#         print(f"tensors['ind_1'] shape: {tensors['ind_1'].shape}")
+#         print(f"tensors['ind_2'] shape: {tensors['ind_2'].shape}")
+#         print(f"tensors['prop'] shape: {tensors['prop'].shape}")
+#         print(f"tensors['dist'] shape: {tensors['dist'].shape}")
+#         return tensors
+
+class PiNet(tf.keras.Model):
+    """Keras model for the PiNet neural network
+
+    Args:
+        tensors: input data (nested tensor from dataset).
+        atom_types (list): elements for the one-hot embedding.
+        pp_nodes (list): number of nodes for pp layer.
+        pi_nodes (list): number of nodes for pi layer.
+        ii_nodes (list): number of nodes for ii layer.
+        en_nodes (list): number of nodes for en layer.
+        depth (int): number of interaction blocks.
+        rc (float): cutoff radius.
+        basis_type (string): type of basis function to use,
+            can be "polynomial" or "gaussian".
+        n_basis (int): number of basis functions to use.
+        gamma (float or array): width of gaussian function for gaussian basis.
+        center (float or array): center of gaussian function for gaussian basis.
+        cutoff_type (string): cutoff function to use with the basis.
+        act (string): activation function to use.
+        preprocess (bool): whether to return the preprocessed tensor.
+    """
+    def __init__(self, atom_types=[1, 6, 7, 8, 9],  rc=4.0, cutoff_type='f1',
+                 basis_type='polynomial', n_basis=4, gamma=3.0, center=None,
+                 pp_nodes=[16, 16], pi_nodes=[16, 16], ii_nodes=[16, 16],
+                 out_nodes=[16, 16], out_units=1, out_pool=False,
+                 act='tanh', depth=4):
+
+        super(PiNet, self).__init__()
+
+        self.depth = depth
+        self.preprocess = PreprocessLayer(atom_types, rc)
+        self.activation = act
+
+        if basis_type == 'polynomial':
+            self.basis_fn = PolynomialBasis(cutoff_type, rc, n_basis)
+        elif basis_type == 'gaussian':
+            self.basis_fn = GaussianBasis(cutoff_type, rc, n_basis, gamma, center)
+
+        self.res_update = [ResUpdate() for i in range(depth)]
+        self.gc_blocks = [GCBlock([], pi_nodes, ii_nodes, activation=act)]
+        self.gc_blocks += [GCBlock(pp_nodes, pi_nodes, ii_nodes, activation=act)
+                           for i in range(depth-1)]
+        self.out_layers = [OutLayer(out_nodes, out_units) for i in range(depth)]
+        self.ann_output =  ANNOutput(out_pool)
+    
+    def train_step(self, tensors):
+        with tf.GradientTape() as tape:
+            pred = self(tensors, training=True)
+            ind = tensors['ind_1']
+            nbatch = tf.reduce_max(ind)+1
+            pred = tf.math.unsorted_segment_sum(pred, ind[:, 0], nbatch)
+            # tf.debugging.assert_shapes([(pred, (nbatch, self.out_layers[0].out_units))], message=f"pred shape is incorrect")
+            loss = self.compiled_loss(tensors['e_data'], pred, regularization_losses=self.losses)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.compiled_metrics.update_state(tensors['e_data'], pred)
+        return {m.name: m.result() for m in self.metrics}
+
+    
+    def test_step(self, tensors):
+        pred = self(tensors, training=False)
+        ind = tensors['ind_1']
+        nbatch = tf.reduce_max(ind)+1
+        pred = tf.math.unsorted_segment_sum(pred, ind[:, 0], nbatch)
+        self.compiled_loss(tensors['e_data'], pred, regularization_losses=self.losses)
+        self.compiled_metrics.update_state(tensors['e_data'], pred)
+        return {m.name: m.result() for m in self.metrics}
+    
+
+    def call(self, tensors):
+        tensors = self.preprocess(tensors)
+        basis = self.basis_fn(tensors['dist'])[:, None, :]
+        # print(f"ind_1 shape: {tensors['ind_1'].shape}")
+        # print(f"ind_2 shape: {tensors['ind_2'].shape}")
+        # print(f"dist shape: {tensors['dist'].shape}")
+        output = 0.0
+        for i in range(self.depth):
+            # prop = self.gc_blocks[i]([tensors['ind_2'], tensors['prop'], basis])
+            # print(f"Before GCBlock {i}, prop shape: {tensors['prop'].shape}")
+            prop = self.gc_blocks[i]([tensors['ind_2'], tensors['prop'], basis])
+            # print(f"After GCBlock {i}, prop shape: {prop.shape}")
+
+            output = self.out_layers[i]([tensors['ind_1'], prop, output])
+            # tf.debugging.assert_shapes([(output, (None, self.out_layers[i].out_units))], message=f"output shape is incorrect at iteration {i}")
+            if output.shape != (300,1):
+                print('Wrong shape!', output.shape)
+            tensors['prop'] = self.res_update[i]([tensors['prop'], prop])
+
+        output = self.ann_output([tensors['ind_1'], output])
+        return output
+
+
+
+
+
+
+
+    # def call(self, tensors):
+    #     tensors = self.preprocess(tensors)
+    #     basis = self.basis_fn(tensors['dist'])[:, None, :]
+    #     output = 0.0
+    #     for i in range(self.depth):
+    #         prop = self.gc_blocks[i]([tensors['ind_2'], tensors['prop'], basis])
+    #         output = self.out_layers[i]([tensors['ind_1'], prop, output])
+    #         print("output shape at step {}: {}".format(i, output.shape))
+    #         if output.shape != (300,1):
+    #             print('Wrong shape!', output.shape)
+    #             #     print('hello output:', output)
+    #             #     sys.exit()
+    #         tensors['prop'] = self.res_update[i]([tensors['prop'], prop])
+    #         print("prop shape at step {}: {}".format(i, tensors['prop'].shape))
+    #         print("ind_1 shape at step {}: {}".format(i, tensors['ind_1'].shape))
+    #         print("ind_2 shape at step {}: {}".format(i, tensors['ind_2'].shape))
+    #         print("dist shape at step {}: {}".format(i, tensors['dist'].shape))
+    #         print("basis shape at step {}: {}".format(i, basis.shape))
+    #     output = self.ann_output([tensors['ind_1'], output])
+    #     print("final output shape: {}".format(output.shape))
+    #     return output
+
+
+
+
+    # ORIGINAL
+    # def call(self, tensors):
+    #     tensors = self.preprocess(tensors)
+    #     basis = self.basis_fn(tensors['dist'])[:, None, :]
+    #     output = 0.0
+    #     for i in range(self.depth):
+    #         prop = self.gc_blocks[i]([tensors['ind_2'], tensors['prop'], basis])
+    #         output = self.out_layers[i]([tensors['ind_1'], prop, output])
+    #         if output.shape != (300,1):
+    #             print('Wrong shape!', output.shape)
+    #         #     print('hello output:', output)
+    #         #     sys.exit()
+    #         tensors['prop'] = self.res_update[i]([tensors['prop'], prop])
+
+    #     output = self.ann_output([tensors['ind_1'], output])
+    #     return output
---
+++ PiNN_scripts/potential.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""This file implements the potential model
+
+Atomic predictions from the network are interpreted as atomic energies. This
+model is also capable of fitting and predicting forces and stresses.
+"""
+import numpy as np
+import tensorflow as tf
+from pinn import get_network
+from pinn.utils import pi_named, atomic_dress, connect_dist_grad
+from pinn.models.base import export_model, get_train_op, MetricsCollector
+import traceback
+
+default_params = {
+    ### Scaling and units # The loss function will be MSE((pred - label) * scale)
+    # For vector/tensor predictions
+    # the error will be pre-component instead of per-atom
+    # e_unit is the unit of energy to report w.r.t the input labels
+    # no f_unit yet, f_unit is just e_unit/input coordinate unit
+    # e.g. if one have input in Hartree, scales it by 100 for training
+    #      and output eV when report error
+    #      then e_scale should be 100, and e_unit = hartree2evp
+    'e_dress': {},  # element-specific energy dress
+    'e_scale': 1.0,  # energy scale for prediction
+    'e_unit': 1.0,  # output unit of energy during prediction
+    # Loss function options
+    ## Energy
+    'max_energy': False,        # if set to float, omit energies larger than it
+    'use_e_per_atom': False,    # use e_per_atom to calculate e_loss
+    'log_e_per_atom': False,    # log e_per_atom and its distribution
+                                # ^- this is forcely done if use_e_per_atom
+    'use_e_weight': False,      # scales the loss according to e_weight
+    ## Force
+    'use_force': False,         # include force in loss function
+    'max_force_comp': False,    # if set to float, omit forces components larger than it
+    'no_force_comp': False,     # if set to int, use as maximum number of force components for a update
+    'use_f_weight': False,      # scales the loss according to f_weights
+    ## Stress
+    'use_stress': False,        # include stress in Loss function
+    ## L2
+    'use_l2': False,
+    ## Loss function multipliers
+    'e_loss_multiplier': 1.0,
+    'f_loss_multiplier': 1.0,
+    's_loss_multiplier': 1.0,
+    'l2_loss_multiplier': 1.0,
+    'separate_errors': False,   # workaround at this point
+}
+
+@export_model
+def potential_model(features, labels, mode, params):
+    """Model function for neural network potentials"""
+    network = get_network(params['network'])
+    model_params = default_params.copy()
+    model_params.update(params['model']['params'])
+
+    # print('print features 1:', features)
+    #traceback.print_stack()
+
+    features = network.preprocess(features)
+    connect_dist_grad(features)
+    pred = network(features)
+    # print('print pred1:', pred)
+    # print('print features 2:', features)
+    # print('print labels:', labels)
+    # print('print params:', params)
+
+
+    ind = features['ind_1']
+    nbatch = tf.reduce_max(ind)+1
+    pred = tf.math.unsorted_segment_sum(pred, ind[:, 0], nbatch) #sums the batches I think
+
+    # print('print pred2', pred)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        metrics = make_metrics(features, pred, model_params, mode)
+        tvars = network.trainable_variables
+        # print('hello metrics:', metrics)
+        # print('metrics.loss:', metrics.LOSS)
+        # print('metrics.error:', metrics.ERROR)
+        train_op = get_train_op(params['optimizer'], metrics, tvars,
+                                separate_errors=model_params['separate_errors'])
+        return tf.estimator.EstimatorSpec(mode, loss=tf.reduce_sum(metrics.LOSS),
+                                          train_op=train_op)
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+        metrics = make_metrics(features, pred, model_params, mode)
+        return tf.estimator.EstimatorSpec(mode, loss=tf.reduce_sum(metrics.LOSS),
+                                          eval_metric_ops=metrics.METRICS)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        pred = pred / model_params['e_scale']
+        if model_params['e_dress']:
+            pred += atomic_dress(features, model_params['e_dress'], dtype=pred.dtype)
+        pred *= model_params['e_unit']
+        forces = -_get_dense_grad(pred, features['coord'])
+        forces = tf.expand_dims(forces, 0)
+        predictions = {'energy': pred, 'forces': forces}
+        if 'cell' in features:
+            stress = _get_stress(pred, features)
+            predictions['stress'] = stress
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+@pi_named("METRICS")
+def make_metrics(features, pred, params, mode):
+    from pinn.utils import count_atoms
+
+    metrics = MetricsCollector(mode)
+
+    e_pred = pred
+    e_data = features['e_data'] #this is the input value (before pinet)
+    if params['e_dress']:
+        e_data -= atomic_dress(features, params['e_dress'], dtype=pred.dtype)
+    e_data *= params['e_scale']
+
+    # should get the mask here since max_energy refers to total energy
+    e_mask = tf.abs(e_data) > params['max_energy'] if params['max_energy'] else None
+    e_weight = params['e_loss_multiplier']
+    e_weight *= features['e_weight'] if params['use_e_weight'] else 1
+    metrics.add_error('E', e_data, e_pred, mask=e_mask, weight=e_weight,
+                      use_error=(not params['use_e_per_atom']))
+
+    if params['use_e_per_atom'] or params['log_e_per_atom']:
+        n_atoms = count_atoms(features['ind_1'], dtype=e_data.dtype)
+        metrics.add_error('E_PER_ATOM', e_data/n_atoms, e_pred/n_atoms, mask=e_mask,
+                          weight=e_weight, use_error=params['use_e_per_atom'],
+                          log_error=params['log_e_per_atom'])
+
+    if params['use_force']:
+        f_pred = -_get_dense_grad(pred, features['coord'])
+        f_data = features['f_data']*params['e_scale']
+        f_mask = tf.fill(tf.shape(f_pred), True)
+
+        if params['max_force_comp']:
+            f_mask = tf.abs(f_data)<params['max_force']
+            f_pred = tf.boolean_mask(f_pred, use_ind)
+            f_data = tf.boolean_mask(f_data, use_ind)
+
+        if params['no_force_comp']:
+            use_ind = tf.cast(tf.random.shuffle(tf.where(f_mask))[:params['no_force_comp']], tf.int32)
+            f_mask = tf.scatter_nd(use_ind, tf.fill(tf.shape(use_ind)[:1],True), tf.shape(f_mask))
+
+        f_weight = params['f_loss_multiplier']
+        f_weight *= features['f_weight'] if params['use_f_weight'] else 1
+        metrics.add_error('F', f_data, f_pred, mask=f_mask, weight=f_weight)
+
+    if params['use_stress']:
+        s_pred = _get_stress(pred, features)
+        s_data = features['s_data']*params['e_scale']
+        metrics.add_error('S', s_data, s_pred, weight=params['s_loss_multiplier'])
+
+    if params['use_l2']:
+        tvars = tf.compat.v1.trainable_variables()
+        l2_vars = tf.concat([
+            tf.reshape(v, [-1]) for v in tvars if
+            ('bias' not in v.name and 'noact' not in v.name)], axis=0)
+        metrics.add_error('L2', l2_vars, 0, weight=params['l2_loss_multiplier'],
+                          log_error=False, log_hist=False)
+
+    return metrics
+
+
+def _get_stress(pred, tensors):
+    f_ij = _get_dense_grad(pred, tensors['diff'])
+    s_pred = tf.reduce_sum(
+        tf.expand_dims(f_ij, 1) *
+        tf.expand_dims(tensors['diff'], 2),
+        axis=0, keepdims=True)
+    s_pred /= tf.linalg.det(tensors['cell'])
+    return s_pred
+
+
+def _get_dense_grad(energy, coord):
+    """get a gradient and convert to dense form"""
+    import warnings
+    index_warning = 'Converting sparse IndexedSlices'
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', index_warning)
+        grad = tf.gradients(energy, coord)[0]
+    if type(grad) == tf.IndexedSlices:
+        grad = tf.scatter_nd(tf.expand_dims(grad.indices, 1), grad.values,
+                             tf.cast(grad.dense_shape, tf.int32))
+    return grad
